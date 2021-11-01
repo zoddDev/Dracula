c137e45435e335a4d0f152f48cab62c68d85206b
zodd18 <josiesala@gmail.com>
2021-11-01 21:41:34 +0100

    Minor changes

--- .config/polybar/theme-swap-theme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-AVAILABLE_THEMES=$(cat $HOME/.config/polybar/scripts/available-themes)
-NUMBER_OF_THEMES=10
-echo -e $AVAILABLE_THEMES | rofi -font "Iosevka Bold 11" -show drun -show-icons -width 20 -lines 10 $NUMBER_OF_THEMES -dmenu -i -config "~/.config/rofi/config.rasi"
-
